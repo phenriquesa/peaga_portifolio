52fee31413379314ae798e533cc42a188625a605
Paulo Henrique <paulohsaragao@hotmail.com>
2022-01-20 05:45:30 -0300

    Update

---
+++ app/static/js/script.js
@@ -0,0 +1,22 @@
+/* ================================================================ */
+
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+
+};
+/* ================================================================ */
+function BlockLetters(e){
+    var tecla=(window.event)?event.keyCode:e.which;
+    if((tecla>47 && tecla<58)) return true;
+    else{
+    	if (tecla==8 || tecla==0) return true;
+	else  return false;
+    }
+};
+/* ================================================================ */
