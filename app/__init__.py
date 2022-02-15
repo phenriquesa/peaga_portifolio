6554f7ac15672d7d88563482bf76e1ac81a079fa
Paulo Henrique Souza Aragão <98068521+phenriquesa@users.noreply.github.com>
2022-02-15 04:32:40 -0300

    Update __init__.py
    Update

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=os.urandom(32),
+        SECRET_KEY=os.urandom(64),
         DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
     )
 
@@ -22,13 +22,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
+# Init DB
     from . import db
     db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
 
     from . import site
     app.register_blueprint(site.bp)
