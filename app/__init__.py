b8bbc0255e5144755ce7008f011121dd19a011be
phenriquesa <paulohsaragao@hotmail.com>
2024-10-30 19:57:36 -0300

    Atualizar config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 import csv
 import os
-from typing import io
 from bs4 import BeautifulSoup
 from datetime import datetime
 import textwrap
@@ -29,6 +28,7 @@ import unicodedata
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        port=8000,
         SECRET_KEY=os.getenv("SECRET_KEY"),
         DATABASE=os.path.join(app.instance_path, "db.sqlite"),
     )
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='app',
-    version='1.3',
+    version='4.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
